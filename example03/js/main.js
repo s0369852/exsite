ffc4c3a7274b2dbb99250ee950c94f1abfed3dce
s0369852 <s0369852@gmail.com>
2021-07-01 22:41:27 +0900

    19. 요소 슬라이드 - 프로모션 이미지 슬라이드(1)

--- example03/js/main.js
+++ example03/js/main.js
@@ -65,4 +65,22 @@ new Swiper('.notice-line .swiper-container', {
   roop: true
 });
 
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true, // 첫번째 슬라이드 왼쪽에 마지막 슬라이드가 있어야 하고, 마지막 슬라이드 오른쪽에 첫번째 슬라이드가 있게 하려면 loop(반복재생)를 이용해줘야 한다.
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable : true, // 사용자의 페이지 번호 요소 제어
+  },
+  navigation : {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
 // // swiper slide
